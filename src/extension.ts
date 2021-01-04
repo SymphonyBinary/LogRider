c8198f09598c2b685ea647537a6636030b687a1d
Derek Yau <derekayau@gmail.com>
2021-01-04 01:00:58 -0800

    command to process it and add colours

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,54 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { TextDecoder } from 'util';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "logrider" is now active!');
+	const scheme = "logRider";
+	const provider = new LogRiderProvider();
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(scheme, provider));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('logrider.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('logrider.openCLog', async () => {
+		const clogURI = await vscode.window.showOpenDialog({
+			openLabel: "Select clog file",
+			canSelectMany: false,
+			filters: { "captainsLog": ['clog']}
+		});
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LogRider!');
+		if(clogURI && clogURI[0]) {
+			const virtualDocURI = vscode.Uri.parse('logRider:' + clogURI[0]);
+			const doc = await vscode.workspace.openTextDocument(virtualDocURI); // calls back into the provider
+			await vscode.window.showTextDocument(doc, { preview: false });
+		}
 	});
 
 	context.subscriptions.push(disposable);
+	
+}
+
+class LogRiderProvider implements vscode.TextDocumentContentProvider {
+	// emitter and its event
+	onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+	onDidChange = this.onDidChangeEmitter.event;
+
+	async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+		//this gets us the entire uri, with the scheme too
+		//return uri.toString();
+
+		const filePath = vscode.Uri.parse(uri.fsPath);
+		const encodedDoc = await vscode.workspace.fs.readFile(filePath);
+
+		let dec = new TextDecoder("utf-8");
+		let doc = dec.decode(encodedDoc);
+
+		var re = /.*:: :  : (.*)/gi;
+		doc = doc.replace(re, "$1");
+
+		return doc.toString();
+	}
 }
 
 // this method is called when your extension is deactivated
