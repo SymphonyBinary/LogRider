040f88fb3e296b04ed0e8f526cbb8211bb466bf4
Derek Yau <derekayau@gmail.com>
2024-03-02 15:45:15 -0800

    refactored processClog to a bit cleaner. Still wip.

--- cpp/processor/src/processClog.cpp
+++ cpp/processor/src/processClog.cpp
@@ -7,6 +7,8 @@
 #include <memory>
 #include <optional>
 #include <vector>
+#include <cassert>
+#include <algorithm>
 
 /*
 ------------------------------------------------------------------------------
@@ -51,27 +53,10 @@ CAP_LOG : P=4293102038 T=0 C=005 :L 3 [25]::[test.cpp]::[something::TestNetwork:
 
 namespace {
 /**
- * This will match all the logs:
- * CAP_LOG_BLOCK, CAP_LOG_BLOCK_NO_THIS, CAP_LOG, CAP_LOG_ERROR, CAP_SET
- * the sub-expressions:
- * 0 - the full string
- * 1 - ProcessId
- * 2 - ThreadId
- * 3 - ChannelId
- * 4 - Indentation
- * 5 - FunctionId
- * 6 - Line number in source code
- * 7 - Info string; the remainder of the string.  Changes depending on log type.
- **/
-std::regex logLineRegex(
-  ".*CAP_LOG : P=(.+?) T=(.+?) C=(.+?) (.+?) (.+?) (\\[.+?\\])(.*)",
-  std::regex_constants::ECMAScript);
-
-/**
  * This will match the opening and closing block tags
  * eg. ::[test.cpp]::[something::TestNetwork::TestNetwork()] 0x7ffecc005730
  * 0 - the full string
- * 1 - Filename
+ * 1 - filename
  * 2 - Function name (may be truncated if too long)
  * 3 - The object id.  In c++ this is the "this" pointer (or 0 if none)
  **/
@@ -83,38 +68,155 @@ std::regex infoStringBlockMatch(
 class WorldState {
 public:    
   struct LoggedObject {
-    std::string thisAddress;
+    std::string objectId;
     std::unordered_map<std::string, std::unordered_map<int, std::string>> pushedVariables;
   };
 
   struct StackNode {
-    int line;
-    int depth;
-    std::string fileName;
-    std::string functionName;
-    std::unique_ptr<StackNode> caller;
+    StackNode(
+      size_t line, 
+      size_t depth, 
+      size_t threadId, 
+      size_t processId, 
+      std::string filename, 
+      std::string functionName,
+      StackNode* caller)
+      : line(line)
+      , depth(depth)
+      , threadId(threadId)
+      , processId(processId)
+      , filename(std::move(filename))
+      , functionName(std::move(functionName))
+      , caller(caller) {}
+
+    const size_t line;
+    const size_t depth;
+    const size_t threadId;
+    const size_t processId;
+    const std::string filename;
+    const std::string functionName;
+    const StackNode* caller;
     std::optional<LoggedObject> loggedObject;
   };
 
+  // should use expected, but that's only in c++23
+  // the objectId is address of the object for logs in c++
+  enum ExistsPolicy{DontCreateIfNotExist, CreateIfNotExist};
+  LoggedObject* getLoggedObject(const std::string& objectId, ExistsPolicy existsPolicy) {
+    if (objectId.empty()) {
+      return nullptr;
+    }
+
+    LoggedObject* retLoggedObject;
+    if (auto loggedObjMapIter = mLoggedObjects.begin(); loggedObjMapIter != mLoggedObjects.end()) {
+      retLoggedObject = loggedObjMapIter->second.get();
+    } else if (existsPolicy == ExistsPolicy::CreateIfNotExist) {
+      auto newLoggedObject = std::make_unique<LoggedObject>();
+      newLoggedObject->objectId = objectId;
+      retLoggedObject = newLoggedObject.get();
+      mLoggedObjects.emplace(objectId, std::move(newLoggedObject));
+    }
+
+    return retLoggedObject;
+  }
+
+  StackNode& pushNewStackNode(
+      size_t line,
+      size_t depth, 
+      size_t threadId, 
+      size_t processId, 
+      std::string filename, 
+      std::string functionName,
+      StackNode* caller) {
+    size_t stackNodeIdx = mStackNodeArray.size();
+    assert(stackNodeIdx == line);
+
+    mStackNodeArray.emplace_back(std::make_unique<StackNode>(
+      line, depth, threadId, processId, std::move(filename), std::move(functionName), caller));
+
+    ThreadIdToStackNodeIdxArray& threadIdToStackNodeIdxArray = 
+      mProcessIdToThreadIdToStackNodeIdxArray[processId];
+    threadIdToStackNodeIdxArray[threadId].emplace_back(stackNodeIdx);
+    
+    return *mStackNodeArray.back().get();
+  }
+
+  StackNode* getStackNodeOnLine(size_t lineNumber) {
+    if (lineNumber < mStackNodeArray.size()) {
+      return mStackNodeArray[lineNumber].get();
+    } else {
+      return nullptr;
+    }
+  }
+
 private:
-  std::unordered_map<std::string, LoggedObject> mLoggedObjects;
-  std::vector<StackNode> mLineData;
+  std::unordered_map<std::string, std::unique_ptr<LoggedObject>> mLoggedObjects;
+  
+  using StackNodeArray = std::vector<std::unique_ptr<StackNode>>;
+  StackNodeArray mStackNodeArray;
+
+  using StackNodeIdxArray = std::vector<size_t>;
+  using ThreadIdToStackNodeIdxArray = std::unordered_map<size_t, StackNodeIdxArray>;
+  using ProcessIdToThreadIdToStackNodeIdxArray = std::unordered_map<size_t, ThreadIdToStackNodeIdxArray>;
+  ProcessIdToThreadIdToStackNodeIdxArray mProcessIdToThreadIdToStackNodeIdxArray;
 };
 
+struct WorldStateWorkingData {
+  size_t nextProcessId = 0;
+  size_t nextThreadId = 0;
+  size_t nextLogLineNumber = 0;
+  std::string inputLine;
+};
 
 struct OutputState {
   std::string outputText;
-  int logLineNumber = 0;
-
-
 };
 
-bool processLogLine(const std::string& inputLine, OutputState& outputState) {
+bool processLogLine(WorldStateWorkingData& workingData, WorldState& worldState, OutputState& outputState) {
+  /**
+   * This will match all the logs:
+   * CAP_LOG_BLOCK, CAP_LOG_BLOCK_NO_THIS, CAP_LOG, CAP_LOG_ERROR, CAP_SET
+   * the sub-expressions:
+   * 0 - the full string
+   * 1 - ProcessId
+   * 2 - ThreadId
+   * 3 - ChannelId
+   * 4 - Indentation
+   * 5 - FunctionId
+   * 6 - Line number in source code
+   * 7 - Info string; the remainder of the string.  Changes depending on log type.
+   **/
+  std::regex logLineRegex(
+    ".*CAP_LOG : P=(.+?) T=(.+?) C=(.+?) (.+?) (.+?) (\\[.+?\\])(.*)",
+    std::regex_constants::ECMAScript);
 
+  std::cmatch pieces_match;
+  bool matched = std::regex_match (workingData.inputLine.c_str(), pieces_match, logLineRegex);
+  if (matched) {
+    // structured bindings don't work for regex match :(
+    // cannot decompose inaccessible member ‘std::__cxx11::match_results<__gnu_cxx::__normal_iterator<...
+    auto&& inputFullMatch = pieces_match[0];
+    auto&& inputProcessId = pieces_match[1];   // 2
+    auto&& inputThreadId = pieces_match[2];    // 3
+    auto&& inputChannelId = pieces_match[3];   // 4
+    auto&& inputIndentation = pieces_match[4]; // 5
+    auto&& inputFunctionId = pieces_match[5];  // 6
+    auto&& inputSourceLine = pieces_match[6];  // 7
+    auto&& inputInfoString = pieces_match[7];  // 8
+
+    std::outputIndentation = inputIndentation;
+    std::replace(outputIndentation.begin(), outputIndentation.end(), ":", "║");
+    std::replace(outputIndentation.begin(), outputIndentation.end(), "F", "║");
+    std::replace(outputIndentation.begin(), outputIndentation.end(), "L", "║");
+    std::replace(outputIndentation.begin(), outputIndentation.end(), "-", "║");
+    std::replace(outputIndentation.begin(), outputIndentation.end(), ">", "║");
+  }
+
+  return matched;
 }
 
-size_t getFileSize(char* fileName) {
-  std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+size_t getFileSize(char* filename) {
+  std::ifstream file(filename, std::ios::binary | std::ios::ate);
   return file.tellg();
 }
 
@@ -125,42 +227,24 @@ int main(int argc, char* argv[]) {
     std::cout << "Usage: processClog [clogfile.clog]";
     return 0;
   }
-  char* inputFileName = argv[1];
+  char* inputfilename = argv[1];
 
-  size_t inputFileSize = getFileSize(inputFileName);
+  size_t inputFileSize = getFileSize(inputfilename);
 
-  std::ifstream fileStream(inputFileName);
+  std::ifstream fileStream(inputfilename);
 
   OutputState output;
   output.outputText.reserve(inputFileSize * 2);
 
+  WorldState worldState;
+  WorldStateWorkingData worldWorkingData;
+
   std::string inputLine;
   while (std::getline(fileStream, inputLine)) {
-    // if (processLogLine)
-
-    std::cmatch pieces_match;
-    bool matched = std::regex_match (inputLine.c_str(), pieces_match, logLineRegex);
-    if (matched) {
-      // structured bindings don't work for regex match :(
-      // cannot decompose inaccessible member ‘std::__cxx11::match_results<__gnu_cxx::__normal_iterator<...
-      auto&& inputFullMatch = pieces_match[0];
-      auto&& inputProcessId = pieces_match[1];   // 2
-      auto&& inputThreadId = pieces_match[2];    // 3
-      auto&& inputChannelId = pieces_match[3];   // 4
-      auto&& inputIndentation = pieces_match[4]; // 5
-      auto&& inputFunctionId = pieces_match[5];  // 6
-      auto&& inputSourceLine = pieces_match[6];  // 7
-      auto&& inputInfoString = pieces_match[7];  // 8
-
-
+    worldWorkingData.inputLine = std::move(inputLine);
+    if (processLogLine(worldWorkingData, worldState, output)) {
+      continue;
     }
-
-
-    // for (std::size_t i = 0; i < pieces_match.size(); ++i) {
-    //   std::ssub_match sub_match = pieces_match[i];
-    //   std::string piece = sub_match.str();
-    //   std::cout << "  submatch " << i << ": " << piece << '\n';
-    // }
   }
 
 
